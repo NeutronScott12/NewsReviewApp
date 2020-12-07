ac6d8ebf6a0027f9596bc0175f4c88820a4952aa
Scott <scottberry91@gmail.com>
2020-12-07 15:24:30 +0000

    Added helmet, csurf, cors and rate limit

--- packages/nestjs_backend/src/main.ts
+++ packages/nestjs_backend/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
+import csurf from 'csurf'
+import rateLimit from 'express-rate-limit'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+    app.enableCors({ origin: '*' })
+    app.use(helmet())
+
+    // @TODO add session and cookie functionality to get this working
+    // app.use(csurf())
+    // app.use(
+    //     rateLimit({
+    //         windowMs: 15 * 60 * 1000,
+    //         max: 100,
+    //     }),
+    // )
+
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
